a2b277145764bb79d2e7131df2d6158dca7bde48
vishalmehta672 <vishalmehta672@gmail.com>
2025-03-01 22:00:14 +0530

    resolve isAnagram

---
+++ Array and Hashing/Valid_Anagram.py
@@ -0,0 +1,23 @@
+def isAnagram(s: str, t: str) -> bool:
+
+    res1 = {}
+    res2 = {}
+    for i in s:
+        if i in res1:
+            res1[i] += 1
+        else:
+            res1[i] = 1
+
+    for i in t:
+        if i in res2:
+            res2[i] += 1
+        else:
+            res2[i] = 1
+
+    return res1 == res2
+
+
+s = "racecar"
+t = "carrace"
+
+print(isAnagram(s, t))
